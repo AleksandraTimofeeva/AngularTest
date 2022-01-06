5d7beb576e25fd0bb31a886ef34e8a878560edbd
atimofeeva <welcomeToApt1>
2022-01-06 11:38:55 +0100

    Add counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit} from '@angular/core';
-import {Subscription, of, fromEvent, merge} from "rxjs";
-import {map, switchMap, takeUntil, skipUntil, distinctUntilChanged, debounceTime, mergeAll} from "rxjs/operators";
-import {ajax} from "rxjs/ajax";
+import { Component, OnInit } from '@angular/core';
+import {fromEvent, interval, timer} from "rxjs";
+import {mapTo, scan, takeUntil, takeWhile, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -12,48 +11,29 @@ export class AppComponent implements OnInit{
   title = 'learn-angular';
 
   ngOnInit(): void {
-    const search: HTMLElement = document.getElementById('search');
-    const content: HTMLElement = document.getElementById('content');
+    const startBtn = document.getElementById('startBtn');
+    const stopBtn = document.getElementById('stopBtn');
+    const input = document.getElementById('input');
 
-    const KEY = 'a8ddda7d272461c6917529bfeb3c0f9b';
-    const BASE_URL = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&list=search&srsearch=';
+    const startClick$ = fromEvent(startBtn, 'click');
+    const stopClick$ = fromEvent(stopBtn, 'click');
 
-    const obs$ = fromEvent(search, 'keyup');
-    obs$
-      .pipe(
-        debounceTime(1000),
-        map(e => {
-          const value = e.target['value'];
-          return ajax.getJSON(`${BASE_URL}${value}`)
-        }),
-        distinctUntilChanged(),
-        mergeAll(),
-        map(res=> res['query']['search'])
-      )
-      .subscribe(console.log);
+    const counter$ = interval(100);
+    const timer$ = timer(10, 1000);
 
-
-    // const dragElement = document.getElementById('drag-element');
-    //
-    // const mouseDown$ = fromEvent(dragElement, 'mousedown');
-    // const mouseMove$ = fromEvent(dragElement, 'mousemove');
-    // const mouseUp$ = fromEvent(dragElement, 'mouseup');
-    //
-    // const mouseDragging$ = mouseMove$.pipe(
-    //   skipUntil(mouseDown$),
-    //   takeUntil(mouseUp$)
-    // );
-    //
-    // //
-    // const mapToBoolean = (bool) => map(() => bool);
-    //
-    // //universal drag event will emit true on drag (desktop/mobile) optional:add touchStart$ to the merge.
-    // const move$ = merge(mouseDragging$, mouseMove$).pipe(mapToBoolean(true);
-    //
-    // //universal end of drag event will emit false on drag end (desktop/mobile)
-    // const end$ = merge(mouseUp$, mouseDown$).pipe(mapToBoolean(false);
-    //
-    // //merged to return true or false depending on user dragg
-    // return merge(move$, end$).pipe(distinctUntilChanged());
-  }
+    startClick$.subscribe(() => {
+      timer$.pipe(
+        mapTo(-1),
+        scan((acc, crr) => acc + crr, 10),
+        takeUntil(stopClick$),
+        takeWhile(value => value >= -1),
+        tap(console.log)
+      ).subscribe((value => {
+        if (value >= 0) {
+          input.innerHTML = value.toString();
+        } else {
+          input.innerHTML = 'Los!';
+        }
+      }));
+    })}
 }
